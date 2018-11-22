93539dd5027ab6f2ed7d12760d52352708a32a7a
philipplgh <philipplgh@gmail.com>
2018-11-22 18:49:38 +0100

    loading state handling for LoadingButton

--- src/components/Widgets/LoadingButton.jsx
+++ src/components/Widgets/LoadingButton.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Button from './Button'
 import { Spinner } from './LoadingAnimations'
 
-const LoadingButton = ({ children, onClick }) => (
+const LoadingButton = ({ children, onClick, loading }) => (
   <Button onClick={onClick}>
-    <Spinner style={{ float: 'left' }} />
+    {loading && <Spinner style={{ float: 'left' }} />}
     <div style={{ float: 'right' }}>{children}</div>
   </Button>
 )
