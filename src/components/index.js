56fd0b44395cd795934c0c313711c564b726e3e7
philipplgh <philipplgh@gmail.com>
2018-11-26 17:44:34 +0100

    export components

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,16 @@
 import Identicon from './Identicon'
 import EthAddress from './EthAddress'
-import { FeeSelector } from './SendTx'
+import { FeeSelector } from './Tx/SendTx'
 import { FormCreateAccount, InputPassword } from './CreateAccount'
 import NodeInfo from './Network/NodeInfo'
 import { Spinner, Pulse } from './Widgets/LoadingAnimations'
 import Button from './Widgets/Button'
 import LoadingButton from './Widgets/LoadingButton'
 import ValidatedField from './Widgets/ValidatedField'
+import ConverterForm from './Tools/EthConverterForm'
+import NetworkChooser from './Network/NetworkChooser'
+import RpcTester from './Network/RPC/RpcTester'
+import TxHistory from './Tx/History'
 
 export {
   Identicon,
@@ -19,5 +23,9 @@ export {
   Pulse,
   Button,
   LoadingButton,
-  ValidatedField
+  ValidatedField,
+  ConverterForm,
+  NetworkChooser,
+  RpcTester,
+  TxHistory
 };
