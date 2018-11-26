add923d4465dfce52f5bec2f3ab874ceeb0de390
philipplgh <philipplgh@gmail.com>
2018-11-26 17:32:17 +0100

    add converter form

---
+++ src/components/Widgets/ConverterForm.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { ValidatedField } from '..'
+
+class ConverterForm extends React.Component {
+  static displayName = 'ConverterForm'
+
+  static propTypes = {
+
+  }
+
+  static defaultProps = {
+
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: '',
+      output: ''
+    }
+  }
+
+  handleInputChange = (input) => {
+    const { converter } = this.props
+    this.setState({
+      input
+    })
+    const converted = converter(input)
+    this.setState({
+      output: converted
+    })
+  }
+
+  render() {
+    const { input, output } = this.state
+    return (
+      <form>
+        Input:
+        <br />
+        <ValidatedField value={input} placeholder="input" onChange={this.handleInputChange} size={70} />
+        <br />
+        Output:
+        <br />
+        <ValidatedField value={output} placeholder="output" size={70} />
+        <br />
+      </form>
+    )
+  }
+}
+
+export default ConverterForm
