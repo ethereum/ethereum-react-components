db5dee045661b0e5c769b40c89d5b690825b4626
Ryan Ghods <ryan@ryanio.com>
2018-12-04 13:02:43 -0800

    WIP Node Info component * Add prop structure * Add story states ToDo: * Styling

--- src/components/Network/NodeInfo/NodeInfo.jsx
+++ src/components/Network/NodeInfo/NodeInfo.jsx
@@ -3,7 +3,7 @@ import moment from 'moment'
 // import PieChart from 'react-minimal-pie-chart'
 import PropTypes from 'prop-types'
 import i18n from '../../../i18n'
-import { Pulse } from '../../Widgets/LoadingAnimations';
+import { Pulse } from '../../Widgets/LoadingAnimations'
 
 import './NodeInfo.scss'
 
@@ -11,7 +11,7 @@ import NodeInfoDot from './NodeInfoDot'
 import NodeInfoBox from './NodeInfoBox'
 
 // FIXME
-const numeral = (val) => {
+const numeral = val => {
   return {
     format: () => `${val}`
   }
@@ -27,7 +27,7 @@ class NodeInfo extends Component {
   }
 
   render() {
-    const { network, active } = this.props
+    const { network, active, remote, local } = this.props
     const { showSubmenu } = this.state
 
     let mainClass = network === 'main' ? 'node-mainnet' : 'node-testnet'
@@ -41,70 +41,24 @@ class NodeInfo extends Component {
         onMouseEnter={() => this.setState({ showSubmenu: true })}
         onMouseLeave={() => this.setState({ showSubmenu: this.state.sticky })}
       >
-        <NodeInfoDot network={network} active={active} />
+        <NodeInfoDot
+          network={network}
+          active={active}
+          remote={remote}
+          local={local}
+        />
 
         {showSubmenu && (
-          <NodeInfoBox />
+          <NodeInfoBox
+            network={network}
+            active={active}
+            remote={remote}
+            local={local}
+          />
         )}
       </div>
     )
   }
 }
 
-/*
-NodeInfo.propTypes = {
-}
-*/
-
-/*
-function mapStateToProps(state) {
-  return {
-    active: state.nodes.active,
-    network: state.nodes.network,
-    remote: state.nodes.remote,
-    local: state.nodes.local,
-    // re-render when connectedPeers or remoteBlockNumber changes
-    connectedPeers: state.nodes.local.sync.connectedPeers, 
-    remoteBlockNumber: state.nodes.remote.blockNumber
-  }
-}
-*/
-
-const NodeInfoWrapper = () => {
-  const data = {
-    active: 'remote',
-    network: 'main',
-    changingNetwork: false,
-    remote: {
-      client: 'infura',
-      blockNumber: 100, // if < 1000 NodeInfo will display "connecting.."
-      timestamp: 0
-    },
-    local: {
-      client: 'geth',
-      blockNumber: 1,
-      timestamp: 0,
-      syncMode: 'fast',
-      sync: {
-        connectedPeers: 0,
-        currentBlock: 1,
-        highestBlock: 0,
-        knownStates: 0,
-        pulledStates: 0,
-        startingBlock: 0
-      }
-    }
-  }
-  return (
-    <NodeInfo {...data} />
-  )
-}
-
-NodeInfoWrapper.displayName = 'NodeInfo'
-
-NodeInfoWrapper.propTypes = {
-  network: PropTypes.oneOf(['main', 'private'])
-}
-
-// export default NodeInfo
-export default NodeInfoWrapper
+export default NodeInfo
--- src/stories/6.network.stories.jsx
+++ src/stories/6.network.stories.jsx
@@ -7,25 +7,158 @@ import NodeInfoBox from '../components/Network/NodeInfo/NodeInfoBox'
 import NetworkChooser from '../components/Network/NetworkChooser'
 import RpcTest from '../components/Network/RPC/RpcTester'
 
+const initial = {
+  active: 'remote',
+  network: 'main',
+  changingNetwork: false,
+  remote: {
+    client: 'infura',
+    blockNumber: 100,
+    timestamp: 0
+  },
+  local: {
+    client: 'geth',
+    blockNumber: 1,
+    timestamp: 0,
+    syncMode: 'fast',
+    sync: {
+      connectedPeers: 0,
+      currentBlock: 0,
+      highestBlock: 0,
+      knownStates: 0,
+      pulledStates: 0,
+      startingBlock: 0
+    }
+  }
+}
+
+const mainRemote = {
+  active: 'remote',
+  network: 'main',
+  changingNetwork: false,
+  remote: {
+    client: 'infura',
+    blockNumber: 100000,
+    timestamp: Date.now() - 500
+  },
+  local: {
+    client: 'geth',
+    blockNumber: 90000,
+    timestamp: Date.now() - 10000,
+    syncMode: 'fast',
+    sync: {
+      connectedPeers: 5,
+      currentBlock: 90000,
+      highestBlock: 95000,
+      knownStates: 1000000,
+      pulledStates: 1000000,
+      startingBlock: 88000
+    }
+  }
+}
+
+const mainRemoteNoSync = {
+  ...mainRemote,
+  local: { ...mainRemote.local, syncMode: 'nosync', sync: { ...initial.sync } }
+}
+
+const mainLocal = {
+  active: 'local',
+  network: 'main',
+  changingNetwork: false,
+  remote: {
+    client: 'infura',
+    blockNumber: 100000,
+    timestamp: Date.now() - 500
+  },
+  local: {
+    client: 'geth',
+    blockNumber: 100100,
+    timestamp: Date.now() - 200,
+    syncMode: 'fast',
+    sync: {
+      connectedPeers: 5,
+      currentBlock: 100100,
+      highestBlock: 100100,
+      knownStates: 1000000,
+      pulledStates: 1000000,
+      startingBlock: 88000
+    }
+  }
+}
+
+const mainRemoteNoPeers = {
+  ...mainRemote,
+  local: {
+    ...mainRemote.local,
+    sync: { ...initial.local.sync }
+  }
+}
+const mainRemoteStartingSync = {
+  ...mainRemote,
+  local: {
+    ...mainRemote.local,
+    sync: { ...initial.local.sync, connectedPeers: 2 }
+  }
+}
+const testRemote = { ...mainRemote, network: 'ropsten' }
+const testLocal = { ...mainLocal, network: 'ropsten' }
+const privateNet = { ...mainLocal, network: 'private' }
+
 storiesOf('Network/Node Info/Dot', module)
-  .add('no connection ', () => <NodeInfoDot />)
-  .add('main network remote', () => <NodeInfoDot network="main" active="remote" />)
-  .add('main network local', () => <NodeInfoDot network="main" active="local" />)
-  .add('private network remote', () => <NodeInfoDot network="private" active="remote" />)
-  .add('private network local', () => <NodeInfoDot network="private" active="local" />)
+  .add('no connection ', () => <NodeInfoDot {...initial} />)
+  .add('main network remote', () => <NodeInfoDot {...mainRemote} />)
+  .add('main network remote nosync', () => (
+    <NodeInfoDot {...mainRemoteNoSync} />
+  ))
+  .add('main network local', () => <NodeInfoDot {...mainLocal} />)
+  .add('test network remote', () => <NodeInfoDot {...testRemote} />)
+  .add('test network local', () => <NodeInfoDot {...testLocal} />)
+  .add('private network local', () => <NodeInfoDot {...privateNet} />)
 
 storiesOf('Network/Node Info/Box', module)
-  .add('no connection ', () => <NodeInfoBox />)
+  .add('no connection ', () => <NodeInfoBox {...initial} />)
+  .add('main network remote', () => <NodeInfoBox {...mainRemote} />)
+  .add('main network remote nosync', () => (
+    <NodeInfoBox {...mainRemoteNoSync} />
+  ))
+  .add('main network remote no peers', () => (
+    <NodeInfoBox {...mainRemoteNoPeers} />
+  ))
+  .add('main network remote starting sync', () => (
+    <NodeInfoBox {...mainRemoteStartingSync} />
+  ))
+
+  .add('main network local', () => <NodeInfoBox {...mainLocal} />)
+  .add('test network remote', () => <NodeInfoBox {...testRemote} />)
+  .add('test network local', () => <NodeInfoBox {...testLocal} />)
+  .add('private network local', () => <NodeInfoBox {...privateNet} />)
 
 storiesOf('Network/Node Info/Full', module)
-  .add('no connection ', () => <NodeInfo />)
-
-storiesOf('Network/Network Chooser', module)
-  .add('default', () => <div style={{width: 200}}> <NetworkChooser /> </div>)
-
-storiesOf('Network/Rpc', module)
-  .add('default', () => (
-    <div style={{ width: 200 }}>
-      <RpcTest />
-    </div>
+  .add('no connection ', () => <NodeInfo {...initial} />)
+  .add('main network remote', () => <NodeInfo {...mainRemote} />)
+  .add('main network remote nosync', () => <NodeInfo {...mainRemoteNoSync} />)
+  .add('main network remote no peers', () => (
+    <NodeInfo {...mainRemoteNoPeers} />
   ))
+  .add('main network remote starting sync', () => (
+    <NodeInfo {...mainRemoteStartingSync} />
+  ))
+
+  .add('main network local', () => <NodeInfo {...mainLocal} />)
+  .add('test network remote', () => <NodeInfo {...testRemote} />)
+  .add('test network local', () => <NodeInfo {...testLocal} />)
+  .add('private network local', () => <NodeInfo {...privateNet} />)
+
+storiesOf('Network/Network Chooser', module).add('default', () => (
+  <div style={{ width: 200 }}>
+    {' '}
+    <NetworkChooser />{' '}
+  </div>
+))
+
+storiesOf('Network/Rpc', module).add('default', () => (
+  <div style={{ width: 200 }}>
+    <RpcTest />
+  </div>
+))
