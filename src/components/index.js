61ed71c62ca6b9dacfad00c2dd967ff36ad9e7a5
Marc Garreau <marcdgarreau@gmail.com>
2019-02-21 17:42:02 -0700

    chore: add mui grid

--- src/components/index.js
+++ src/components/index.js
@@ -1,38 +1,43 @@
-import Identicon from './Identicon'
 import EthAddress from './EthAddress'
-import FeeSelector from './Tx/SendTx/FeeSelector'
+import Grid from './Grid'
+import Identicon from './Identicon'
 import { FormCreateAccount, InputPassword } from './CreateAccount'
+
+import AddressInput from './Widgets/Form/AddressInput'
+import AddressSelect from './Widgets/Form/AddressSelect'
+import Button from './Widgets/Button'
+import Checkbox from './Widgets/Checkbox'
+import FileChooser from './Widgets/Form/FileChooser'
+import Input from './Widgets/Form/Input'
+import Progress from './Widgets/Form/Progress'
+import Pulse from './Widgets/AnimatedIcons/Pulse'
+import RadioButton from './Widgets/Form/RadioButton'
+import Select from './Widgets/Form/Select'
+import Spinner from './Widgets/AnimatedIcons/Spinner'
+import TextArea from './Widgets/Form/TextArea'
+import ValidatedField from './Widgets/Form/ValidatedField'
+
+import NetworkChooser from './Network/NetworkChooser'
 import NodeInfo from './Network/NodeInfo'
 import NodeInfoBox from './Network/NodeInfo/NodeInfoBox'
 import NodeSettings from './Network/NodeSettings'
-import Spinner from './Widgets/AnimatedIcons/Spinner'
-import Pulse from './Widgets/AnimatedIcons/Pulse'
-import Button from './Widgets/Button'
-import WalletButton from './Widgets/WalletButton'
-import Checkbox from './Widgets/Checkbox'
-import AddressSelect from './Widgets/Form/AddressSelect'
-import RadioButton from './Widgets/Form/RadioButton'
-import Input from './Widgets/Form/Input'
-import AddressInput from './Widgets/Form/AddressInput'
-import TextArea from './Widgets/Form/TextArea'
-import Progress from './Widgets/Form/Progress'
-import Select from './Widgets/Form/Select'
-import FileChooser from './Widgets/Form/FileChooser'
-import ValidatedField from './Widgets/Form/ValidatedField'
-import ConverterForm from './Tools/EthConverterForm'
-import NetworkChooser from './Network/NetworkChooser'
 import RpcTester from './Network/RPC/RpcTester'
-import TxHistory from './Tx/TxHistory'
-import SendTxForm from './Tx/SendTx'
 
-import AccountList from './Wallet/AccountList'
+import FeeSelector from './Tx/SendTx/FeeSelector'
+import SendTxForm from './Tx/SendTx'
+import TxHistory from './Tx/TxHistory'
+
 import AccountItem from './Wallet/AccountItem'
-import TokenListForItem from './Wallet/TokenListForItem'
+import AccountList from './Wallet/AccountList'
 import NavbarBalance from './Wallet/NavbarBalance'
 import NavbarItem from './Wallet/NavbarItem'
 import NetworkStatus from './Wallet/NetworkStatus'
 import Notification from './Wallet/Notification'
 import TokenCard from './Wallet/TokenCard'
+import TokenListForItem from './Wallet/TokenListForItem'
+import WalletButton from './Widgets/WalletButton'
+
+import ConverterForm from './Tools/EthConverterForm'
 
 import * as utils from '../lib/util'
 
@@ -52,6 +57,7 @@ export {
   EthAddress,
   FeeSelector,
   FormCreateAccount,
+  Grid,
   Identicon,
   InputPassword,
   NavbarBalance,
